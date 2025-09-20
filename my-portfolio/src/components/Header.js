3dad8a32d28f6798af712ca90fa016513900a239
chdgld <richard.kalim@msugensan.edu.ph>
2025-09-20 19:31:03 +0800

    jhpoipoi

--- my-portfolio/src/components/Header.js
+++ my-portfolio/src/components/Header.js
@@ -36,7 +36,7 @@ const Header = () => {
         </div>
         
         <nav className={`nav-menu ${isOpen ? 'active' : ''}`}>
-          <button onClick={() => scrollToSection('home')}>kulot</button>
+          <button onClick={() => scrollToSection('home')}>kulot2</button>
           <button onClick={() => scrollToSection('about')}>ABOUT</button>
           <button onClick={() => scrollToSection('projects')}>PROJECTS</button>
           <button onClick={() => scrollToSection('skills')}>SKILLS</button>
